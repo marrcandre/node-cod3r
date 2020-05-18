02e2474b68379d0f1e0853c6eeb13270f435e799
Marco André Mendes <marcoandre@gmail.com>
2020-05-18 18:56:22 -0300

    Teste uso git no vscode

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function helloWorld()
     console.log("Hello World!");
 }
 
+//Função saudação
 const saudacao = () => {
     return data.getHours() <= 12? "Bom dia!": data.getHours() <= 18? "Boa tarde!": "Boa noite!";
     var data = new Date()
